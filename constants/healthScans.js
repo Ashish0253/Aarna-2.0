b5e1900bac91195cd0121bfc962e869fbc3cbac6
vishu bansal <bansalvishu56@gmail.com>
2023-04-12 00:41:34 +0530

    made scans

---
+++ constants/healthScans.js
@@ -0,0 +1,52 @@
+const content = [
+  {
+    id: 1,
+    page: "/ctscan",
+    src: "https://helma.healthians.com/stationery/mailer-assets/61bc3c2d80996.png",
+    atl: "",
+    title: "CT Scan",
+    price: "Starting @",
+    price2: "₹3000",
+    price3: "₹900",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu volutpat odio facilisis mauris.",
+  },
+  {
+    id: 1,
+    page: "/ultrasound",
+    src: "https://helma.healthians.com/stationery/mailer-assets/61bc43a2774ff.png",
+    atl: "",
+    title: "Ultrasound",
+    price: "Starting @",
+    price2: "₹6000",
+    price3: "₹1500",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu volutpat odio facilisis mauris.",
+  },
+  {
+    id: 1,
+    page: "/digitalxray",
+    src: "https://helma.healthians.com/stationery/mailer-assets/61bc45e8a7ad0.png",
+    atl: "",
+    title: "Digital X-Ray",
+    price: "Starting @",
+    price2: "₹1200",
+    price3: "₹500",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu volutpat odio facilisis mauris.",
+  },
+  {
+    id: 1,
+    page: "/ecgtest",
+    src: "https://helma.healthians.com/stationery/mailer-assets/61bc4a3f62b07.jpg",
+    atl: "",
+    title: "ECG Test",
+    price: "Starting @",
+    price2: "₹2000",
+    price3: "₹1500",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu volutpat odio facilisis mauris.",
+  },
+];
+
+export default content;
