05f14073533a1ce2d32dad4c347b3cca9e875f2c
vishu bansal <bansalvishu56@gmail.com>
2023-04-15 22:00:07 +0530

    Scan form like healthians

--- components/ScanForm.js
+++ components/ScanForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 import popup from "../public/popUpimg.jpg";
+import Person from "../public/healthyPerson.png";
 
 export default function ScanForm(props) {
   const [name, setName] = useState("");
@@ -37,6 +38,10 @@ export default function ScanForm(props) {
 
   return (
     <div className="rounded-xl overflow-hidden">
+      <div className=" bg-[#fcf6e4] text-sm pt-6 font-medium text-black flex justify-start items-start px-3">
+        <Image src={Person} height={80} width={80} className="" alt=""></Image>
+        <p className="py-3">{"'"}I only trust Aarna for all my scans.{"'"}</p>
+      </div>
       <h1 className="text-center px-10 bg-[#fcbe57] font-semibold text-lg">
         Get a call back
       </h1>
@@ -52,7 +57,7 @@ export default function ScanForm(props) {
             required
             onChange={(e) => setName(e.target.value)}
             value={name}
-            className="mt-6 placeholder:text-gray-500 border-[1px] border-gray-300 bg-gray-100 px-4"
+            className="placeholder:text-sm mt-6 placeholder:text-gray-500 border-[1px] border-gray-300 bg-gray-100 px-4"
           />
         </div>
         <div className="flex justify-center items-center">
@@ -62,7 +67,7 @@ export default function ScanForm(props) {
             required
             onChange={(e) => setNumber(e.target.value)}
             value={phone}
-            className="mt-6 placeholder:text-gray-500 border-[1px] border-gray-300 bg-gray-100 px-4"
+            className="placeholder:text-sm mt-6 placeholder:text-gray-500 border-[1px] border-gray-300 bg-gray-100 px-4"
           />
         </div>
         <div className="flex justify-center items-center">
@@ -72,7 +77,7 @@ export default function ScanForm(props) {
             required
             // onChange={(e) => setName(e.target.value)}
             value={props.scan}
-            className="mt-6 placeholder:text-black border-[1px] border-gray-300 bg-gray-100 px-4"
+            className="placeholder:text-sm mt-6 placeholder:text-black border-[1px] border-gray-300 bg-gray-100 px-4"
           />
         </div>
         <div className="py-8 flex justify-center items-center">
